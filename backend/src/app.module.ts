847eceb6f7b2582608a992de1443c791769a9a24
ncq01112006 <nc.quang0111@gmail.com>
2026-05-05 21:30:47 +0700

    init cấu trúc BE

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   imports: [
@@ -27,6 +29,10 @@ import { AppService } from './app.service';
         },
       }),
     }),
+
+    PostsModule,
+
+    CommentsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
