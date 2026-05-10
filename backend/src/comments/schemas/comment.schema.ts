c25c86148c32ebdac82c59a5bdf3a0f6e7dffaad
ThinhNguyen14 <thinhnguyen14@gmail.com>
2026-05-10 22:12:44 +0700

    update schame

--- backend/src/comments/schemas/comment.schema.ts
+++ backend/src/comments/schemas/comment.schema.ts
@@ -1,1 +1,26 @@
-export class Comment {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { User } from '../../users/schemas/user.schema';
+import { Post } from '../../posts/schemas/post.schema';
+
+export type CommentDocument = HydratedDocument<Comment>;
+
+@Schema({ timestamps: true })
+export class Comment {
+  @Prop({ required: true })
+  content: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  author: User;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true })
+  post: Post;
+
+  @Prop({ default: 0 })
+  upvotes: number;
+
+  @Prop({ default: 0 })
+  downvotes: number;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
