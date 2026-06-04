5517fa64fdfa814c410e889b411283b5ffd3f744
ncq01112006 <nc.quang0111@gmail.com>
2026-06-04 12:24:59 +0700

    update

--- frontend/config/defaultSettings.ts
+++ frontend/config/defaultSettings.ts
@@ -7,7 +7,7 @@ const Settings: LayoutSettings & {
 	siderWidth: number;
 } = {
 	navTheme: 'light',
-	primaryColor: process.env.APP_CONFIG_PRIMARY_COLOR,
+	primaryColor: process.env.APP_CONFIG_PRIMARY_COLOR || '#2F54EB',
 	borderRadiusBase: '2px',
 	layout: 'mix',
 	contentWidth: 'Fluid',
